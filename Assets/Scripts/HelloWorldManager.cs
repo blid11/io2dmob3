3b37431aec030a9f80d424e902e4bb8e86ae114a
Brett Liddell <brettliddell08@gmail.com>
2022-12-25 12:23:07 +1000

    Made Input Manager a singleton class
    Allowed integration of GUI (buttons) and touch controls.

--- Assets/Scripts/HelloWorldManager.cs
+++ Assets/Scripts/HelloWorldManager.cs
@@ -6,6 +6,30 @@ namespace HelloWorld
 {
     public class HelloWorldManager : MonoBehaviour
     {
+        private InputManager inputManager; 
+        private Camera cameraMain; 
+
+        private void Awake() {
+            inputManager = InputManager.Instance;
+            cameraMain = Camera.main; 
+        }
+
+        private void OnEnable() {
+            inputManager.OnStartTouch += Move;
+        }
+
+        private void OnDisable() {
+            inputManager.OnEndTouch -= Move; 
+        }
+
+        public void Move(Vector2 screenPosition, float time) {
+            /* Vector3 screenCoordinates = new Vector3(screenPosition.x, screenPosition.y, cameraMain.nearClipPlane);
+            Vector3 worldCoordinates = cameraMain.ScreenToWorldPoint(screenCoordinates); 
+            worldCoordinates.z = 0; 
+            transform.position = worldCoordinates;  */
+            Debug.Log("Touch started ");
+        }   
+
         void OnGUI()
         {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
